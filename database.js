04a02ac0b45433cf254905580d0b4af1d35e43c3
kingcoldman <colmankingston@gmail.com>
2026-07-13 19:43:48 +0100

    Create database.js

---
+++ database.js
@@ -0,0 +1,31 @@
+// database.js
+const brawlersDatabase = [
+    {
+        id: "shelly",
+        name: "Shelly",
+        rarity: "Common",
+        img: "https://media.brawltime.ninja/brawlers/shelly/avatar.png",
+        gadgets: ["Fast Forward", "Clay Pigeons"],
+        starPowers: ["Shell Shock", "Band-Aid"],
+        hypercharge: "Double Barrel"
+    },
+    {
+        id: "colt",
+        name: "Colt",
+        rarity: "Common",
+        img: "https://media.brawltime.ninja/brawlers/colt/avatar.png",
+        gadgets: ["Speedloader", "Silver Bullet"],
+        starPowers: ["Slick Boots", "Magnum Special"],
+        hypercharge: "Dual Wield"
+    },
+    {
+        id: "mortis",
+        name: "Mortis",
+        rarity: "Mythic",
+        img: "https://media.brawltime.ninja/brawlers/mortis/avatar.png",
+        gadgets: ["Combo Spinner", "Survival Shovel"],
+        starPowers: ["Creepy Harvest", "Coiled Snake"],
+        hypercharge: "Blood Feast"
+    }
+    // Continue adding your favorite characters following this exact format up to 107!
+];
